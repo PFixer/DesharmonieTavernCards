3c5061835ad165f0051859eb0ab7cc9ad19e246b
PFixer <99512122+PFixer@users.noreply.github.com>
2025-11-20 00:21:07 +0000

    Update start.DjhIgl6y.js

--- build/_app/immutable/entry/start.DjhIgl6y.js
+++ build/_app/immutable/entry/start.DjhIgl6y.js
@@ -1,1 +1,1 @@
-import{l as o,a as r}from"../chunks/Mp0MXOF7.js";export{o as load_css,r as start};
+import{l as o,a as r}from"https://pfixer.github.io/DesharmonieTavernCards/chunks/Mp0MXOF7.js";export{o as load_css,r as start};
